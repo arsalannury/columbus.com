eda8d417d3a07ed7f505a8d64dce6501fe36df2d
arsalannury <arsiiiland@yahoo.com>
2022-09-20 14:00:52 +0430

    add card component

---
+++ columbus/src/components/Card/Card.page.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { Row,Col,Card } from "react-bootstrap"
+import "./card.css"
+
+
+const CardPage: React.FC = () => {
+  return (
+    <>
+      <Row xs={1} md={2} className="g-4">
+        <Col className="w-100">
+          <Card className="flex-row my-3 card-main rounded-0">
+            <Card.Img variant="top" src="favicon.ico" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+    </Row>
+    </>
+  )
+}
+
+export default CardPage
--- columbus/src/components/Columbus/Columbus.page.tsx
+++ columbus/src/components/Columbus/Columbus.page.tsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import Container from 'react-bootstrap/esm/Container'
-import { Row,Col } from 'react-bootstrap'
-import './columbus.css'
+import React from "react";
+import Container from "react-bootstrap/esm/Container";
+import { Row, Col } from "react-bootstrap";
+import "./columbus.css";
+import CardPage from "../Card/Card.page";
 
 const ColumbusPage: React.FC = () => {
   return (
     <>
-    <Container fluid className='columbus_container '>
-         <Row className='columbus_row'>
-            <Col lg={6}>
-               <section className='introduce_setction'>
-                  <h1>Columbus.com</h1>
-                  <p className='main_title'>search about all countries</p>
-                  <p>copyright 2022-2023</p>
-               </section>
-            </Col>
-            <Col lg={6}>
-               ddd
-            </Col>
-         </Row>
-    </Container>
+      <Container fluid className="columbus_container ">
+        <Row className="columbus_row">
+          <Col lg={6}>
+            <section className="introduce_setction">
+              <h1>Columbus.com</h1>
+              <p className="main_title">search about all countries</p>
+              <p>copyright 2022-2023</p>
+            </section>
+          </Col>
+          <Col lg={6}>
+           
+                <CardPage />
+            
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default ColumbusPage
+export default ColumbusPage;
