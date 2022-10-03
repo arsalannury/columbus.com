ff31e4fc383601965df15df0f93bb9607927aedc
arsalannury <arsiiiland@yahoo.com>
2022-10-03 10:48:21 +0330

    [dev] add new interface and loading to detail

--- columbus/src/components/Country/Country.page.tsx
+++ columbus/src/components/Country/Country.page.tsx
@@ -1,13 +1,21 @@
 import { useParams } from "react-router-dom";
 import { useCountryData } from "../../hooks/fetchCountryData";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Spinner } from "react-bootstrap";
 import ErrorBoundryPage from "../ErrorBoundry/ErrorBoundry.page";
-import './country.css';
+import "./country.css";
 
 const CountryPage: React.FC = () => {
   const { id } = useParams();
   const { data, isLoading, isError } = useCountryData(id);
   console.log(data);
+
+  if (isLoading) {
+    return (
+      <div className="d-flex align-items-center justify-content-center w-100 h-100">
+        <Spinner animation="border" variant="light" />
+      </div>
+    );
+  }
 
   if (isError) {
     return <ErrorBoundryPage />;
@@ -15,13 +23,13 @@ const CountryPage: React.FC = () => {
 
   return (
     <>
-    <Container fluid className="container d-flex align-items-center justify-content-center h-100">
-       <div className="country-container">
-<h1>gesgeef</h1>
-       </div>
-    </Container>
+      <div className="d-flex align-items-center justify-content-center h-100">
+        <div className="country-container">
+          <h1>gesgeef</h1>
+        </div>
+      </div>
     </>
-  )
+  );
 };
 
 export default CountryPage;
