f97a334b484c944bd04fe4efde9e41458dfcffd7
arsalannury <arsiiiland@yahoo.com>
2022-10-04 15:44:07 +0330

    [dev] scroll completed

--- columbus/src/components/Columbus/Columbus.page.tsx
+++ columbus/src/components/Columbus/Columbus.page.tsx
@@ -1,28 +1,27 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useRef } from "react";
 import Container from "react-bootstrap/esm/Container";
 import { Row, Col } from "react-bootstrap";
 import "./columbus.css";
 import CardPage from "../Card/Card.page";
 import { useAllData } from "../../hooks/fetchAllData";
 import { Spinner } from "react-bootstrap";
-import { AxiosError, AxiosResponse } from "axios";
+import { AxiosResponse } from "axios";
 import ErrorBoundryPage from "../ErrorBoundry/ErrorBoundry.page";
 
 const ColumbusPage: React.FC = () => {
-  const { data, isError, isLoading, isFetching, error } = useAllData();
-  // const [position,setPosition] = useState<number>(0);
+  const columnRef: any = useRef(null);
+  const { data, isError, isLoading, isFetching } = useAllData();
   const dataType = data as AxiosResponse;
-  const handleScrollSave = (scroll:number): void => {
-      // setPosition(scroll);
-      localStorage.setItem("scrollTop",JSON.stringify(scroll));
-  }  
-  
+  const handleScrollSave = (scroll: number): void => {
+    localStorage.setItem("scrollTop", JSON.stringify(scroll));
+  };
 
   useEffect(() => {
-    const scrollTopToNumber = JSON.parse(localStorage.getItem("scrollTop")!);
-    const columnElement = document.getElementById("#card-column") as HTMLDivElement;
-    if(scrollTopToNumber) {
-      columnElement.scrollTop = scrollTopToNumber;
+    if (localStorage.getItem("scrollTop") && columnRef) {
+      columnRef.current.scrollTo(
+        0,
+        JSON.parse(localStorage.getItem("scrollTop")!)
+      );
     }
   }, []);
 
@@ -50,11 +49,14 @@ const ColumbusPage: React.FC = () => {
               <p>copyright 2022-2023</p>
             </section>
           </Col>
+
           <Col
-            onScroll={(event: any) => {handleScrollSave(event.target.scrollTop)}}
+            onScroll={(event: any) => {
+              handleScrollSave(event.target.scrollTop);
+            }}
             lg={6}
-            id="card-column"
-            style={{ overflowY: "scroll", height: "100%", zIndex: "1" }}
+            ref={columnRef}
+            className="column-scrollable"
           >
             {dataType.data.map((country: any, index: number) => (
               <CardPage
