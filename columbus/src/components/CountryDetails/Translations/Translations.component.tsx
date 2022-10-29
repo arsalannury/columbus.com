fc7c02e719a546f7992ff5e162dde0ab995862a0
arsalannury <arsiiiland@yahoo.com>
2022-10-29 16:21:17 +0330

    ✨ 🎨 add select translation country

--- columbus/src/components/CountryDetails/Translations/Translations.component.tsx
+++ columbus/src/components/CountryDetails/Translations/Translations.component.tsx
@@ -1,18 +1,24 @@
-import Form from "react-bootstrap/Form";
 import "./translations.css";
 
 const TranslationsComponent = ({ translations }: any) => {
-  const translationsToArray = Object.values(translations);
+  const translationsToArray = translations && Object.values(translations);
 
   return (
     <>
-      <Form.Select size="sm" className="custom-select">
-        <option className="custom-option" selected disabled>Translation</option>
-        {translationsToArray !== undefined &&
-          translationsToArray.map((object: any, index: number) => (
-            <option className="custom-option" disabled>{object.official}</option>
-          ))}
-      </Form.Select>
+      <div className="select-wrapper w-100 d-flex align-items-center justify-content-center position-relative">
+        <i
+          className={`icon-select bi bi-caret-down-fill text-black position-absolute`}
+        ></i>
+        <select className="custom-select">
+          <option selected disabled>
+            Translation
+          </option>
+          {translationsToArray !== undefined &&
+            translationsToArray.map((object: any, index: number) => (
+              <option key={index} disabled>{object.official}</option>
+            ))}
+        </select>
+      </div>
     </>
   );
 };
