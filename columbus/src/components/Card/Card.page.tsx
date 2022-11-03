ad6f80917d72662005d3b9ce5c8b03afcfc5464e
arsalannury <arsiiiland@yahoo.com>
2022-11-03 15:36:39 +0330

    💄 fixed bug at card

--- columbus/src/components/Card/Card.page.tsx
+++ columbus/src/components/Card/Card.page.tsx
@@ -16,7 +16,7 @@ const CardPage: React.FC<CardProps> = ({
   translations,
   population,
   region,
-  common
+  common,
 }) => {
   const languageArray: any[] = languages && Object.entries(languages);
 
@@ -24,63 +24,69 @@ const CardPage: React.FC<CardProps> = ({
     <>
       <Row xs={1} md={2} className="g-4 row-card">
         <Col className="w-100">
-          <Link style={{ all: "unset",cursor:"pointer" }} to={`/country/${common}`}>
-            <Card className="flex-row mt-3 card-main rounded-0 overflow-hidden">
+          <Card className="flex-row mt-3 card-main rounded-0 overflow-hidden">
+            <Link
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                cursor: "pointer",
+              }}
+              to={`/country/${common}`}
+            >
               <Card.Img
                 variant="top"
                 src={coatOfArms.png ? coatOfArms.png : flags?.png}
                 loading="lazy"
                 className="card-img"
               />
-              <Card.Body>
-                <Card.Title className="text-muted">{officialName}</Card.Title>
-                <Card.Title className="text-muted">
-                  {translations.per?.official}
-                </Card.Title>
-                <div className="d-flex align-items-end justify-content-end mb-2">
-                  <a rel="noreferrer" target="_blank" href={maps.googleMaps}>
-                    <img
-                      className="maps-icon"
-                      src="map-icon.png"
-                      alt="icon-map-png"
-                    />
-                  </a>
-                </div>
-                <div>
-                  <i className="bi bi-translate text-dark"></i>
-                  {languageArray !== undefined &&
-                    languageArray.map((array: any[], index: number) => {
-                      return (
-                        <span
-                          key={index}
-                          className="text-muted language-parag px-2"
-                        >
-                          {array[1]}
-                        </span>
-                      );
-                    })}
-                </div>
-                <div>
-                  <p className="text-muted language-parag mt-2">
-                    <i
-                      style={{ marginRight: "7px" }}
-                      className="bi bi-globe"
-                    ></i>
-                    {region}
-                  </p>
-                  <p className="text-muted independent-parag">
-                    independent:{" "}
-                    <Badge pill bg={independent ? "success" : "danger"}>
-                      {Helper.ConvertBooleanToYesNo(independent)}
-                    </Badge>
-                  </p>
-                  <p className="text-muted independent-parag">
-                    population: {Helper.SortNumbers(population)}
-                  </p>
-                </div>
-              </Card.Body>
-            </Card>
-          </Link>
+            </Link>
+
+            <Card.Body>
+              <Card.Title className="text-muted">{officialName}</Card.Title>
+              <Card.Title className="text-muted">
+                {translations.per?.official}
+              </Card.Title>
+              <div className="d-flex align-items-end justify-content-end mb-2">
+                <a rel="noreferrer" target="_blank" href={maps.googleMaps}>
+                  <img
+                    className="maps-icon"
+                    src="map-icon.png"
+                    alt="icon-map-png"
+                  />
+                </a>
+              </div>
+              <div>
+                <i className="bi bi-translate text-dark"></i>
+                {languageArray !== undefined &&
+                  languageArray.map((array: any[], index: number) => {
+                    return (
+                      <span
+                        key={index}
+                        className="text-muted language-parag px-2"
+                      >
+                        {array[1]}
+                      </span>
+                    );
+                  })}
+              </div>
+              <div>
+                <p className="text-muted language-parag mt-2">
+                  <i style={{ marginRight: "7px" }} className="bi bi-globe"></i>
+                  {region}
+                </p>
+                <p className="text-muted independent-parag">
+                  independent:{" "}
+                  <Badge pill bg={independent ? "success" : "danger"}>
+                    {Helper.ConvertBooleanToYesNo(independent)}
+                  </Badge>
+                </p>
+                <p className="text-muted independent-parag">
+                  population: {Helper.SortNumbers(population)}
+                </p>
+              </div>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </>
